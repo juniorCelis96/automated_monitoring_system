22414773f89111e471f4b378bde66b3a111425f7
juniorCelis96 <jr.main.396@gmail.com>
2025-02-27 01:36:27 -0500

    - JC: Fix:type: Ajustes de la configuración del bot

---
+++ api_chat_bot_telegram/main.py
@@ -0,0 +1,17 @@
+from config import *
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+application = ApplicationBuilder().token(TOKEN_API_TELEGRAM).build()
+
+async def say_hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hola, ¿cómo estás?")
+
+async def report_history(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(context.args)
+    await update.message.reply_text(context.args[0])
+
+application.add_handler(CommandHandler("start", say_hello))
+application.add_handler(CommandHandler("buscar", report_history))
+
+application.run_polling(allowed_updates=Update.ALL_TYPES)
